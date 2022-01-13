c3c2390b7b9469197e37f57276b13e0fb44bd791
kangil2 <leki305@gmail.com>
2022-01-13 17:03:33 +0900

    [Add] project initial modeling

---
+++ students/kangil/westagram/users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class User(models.Model):
+    name           = models.CharField(max_length=50)
+    email          = models.EmailField(max_length=50)
+    password       = models.CharField(max_length=50)
+    contact_number = models.CharField(max_length=50)
+    
+    class Meta:
+        db_table = "users"
