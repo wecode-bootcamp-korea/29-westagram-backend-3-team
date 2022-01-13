857716c1a0322ded1187f32f2fcb581316e37bdf
kangil2 <leki305@gmail.com>
2022-01-13 17:23:13 +0900

    [Modify] edit column and add property

--- students/kangil/westagram/users/models.py
+++ students/kangil/westagram/users/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class User(models.Model):
     name           = models.CharField(max_length=50)
-    email          = models.EmailField(max_length=50)
-    password       = models.CharField(max_length=50)
+    email          = models.EmailField(max_length=50, unique=True)
+    password       = models.CharField(max_length=200)
     contact_number = models.CharField(max_length=50)
     created_at     = models.DateTimeField(auto_now_add=True)
     updated_at     = models.DateTimeField(auto_now=True)
