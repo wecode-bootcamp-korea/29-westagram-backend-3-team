b934140e5fe05303867ef9a979d959de51dbdad6
11292021 <xforwalkers@gmail.com>
2022-01-13 16:52:08 +0900

    Edit: Update Model to the standard

--- students/hwijeong/westagram/users/models.py
+++ students/hwijeong/westagram/users/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-# Create your models here.
 class user(models.Model):
     name = models.CharField(max_length=30)
     email = models.EmailField(max_length=300, unique=True)
-    password = models.CharField(max_length=100)
-    contact = models.IntegerField(max_length=30, unique=True)
+    password = models.CharField(max_length=200)
+    contact = models.CharField(max_length=30, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
