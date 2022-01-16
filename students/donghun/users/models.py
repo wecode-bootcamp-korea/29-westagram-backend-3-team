94061507d5b85a9a0caf5fe3a27731777af3628a
lee <higgs95@naver.com>
2022-01-16 12:30:51 +0900

    [이동훈] Convention correcting

--- students/donghun/users/models.py
+++ students/donghun/users/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    name = models.CharField(max_length=20)
-    email = models.CharField(max_length=50, unique=True)
+    name     = models.CharField(max_length=20)
+    email    = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=200)
-    contact = models.IntegerField(max_length=20, unique=True)
+    contact  = models.IntegerField(max_length=20, unique=True)
 
     class Meta:
-        db_table = "user"
+        db_table = "users"
