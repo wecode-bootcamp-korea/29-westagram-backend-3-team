3340d6334ce670ee56a15d421ad289d6918bacf2
11292021 <xforwalkers@gmail.com>
2022-01-16 12:24:28 +0900

    Edit: Error handling Logic in Views and Models

--- students/hwijeong/westagram/users/models.py
+++ students/hwijeong/westagram/users/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-from .validators import isEmailValid, isPasswordValid, checkEmailAndPassword
-
 class User(models.Model):
-    name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=255, unique=True, validators=[isEmailValid, checkEmailAndPassword])
-    password = models.CharField(max_length=200, validators=[isPasswordValid, checkEmailAndPassword])
-    contact = models.CharField(max_length=30, unique=True)
+    name       = models.CharField(max_length=30)
+    email      = models.EmailField(max_length=255, unique=True)
+    password   = models.CharField(max_length=200)
+    contact    = models.CharField(max_length=30, unique=True)
+    note       = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
