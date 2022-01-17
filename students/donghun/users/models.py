87f24b68ec0852464a7b57308802216d39f0a444
lee <higgs95@naver.com>
2022-01-17 17:54:52 +0900

    [이동훈] Mission 3: 회원가입 기능 구현

--- students/donghun/users/models.py
+++ students/donghun/users/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     name     = models.CharField(max_length=20)
     email    = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=200)
-    contact  = models.IntegerField(max_length=20, unique=True)
+    contact  = models.CharField(max_length=100, unique=True)
 
     class Meta:
         db_table = "users"
