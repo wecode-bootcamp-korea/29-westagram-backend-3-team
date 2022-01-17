dd015e7c8d5fa4053270d1ad4cfd225c93250931
kangil2 <leki305@gmail.com>
2022-01-17 15:53:13 +0900

    [Add] 로그인 기능 구현

--- students/kangil/westagram/users/views.py
+++ students/kangil/westagram/users/views.py
@@ -9,18 +9,18 @@ class SignUpView(View):
     def post(self, request):
         try:
             user_data           = json.loads(request.body)
-            EMAIL_VALIDATION    = re.compile(r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-            PASSWORD_VALIDATION = re.compile(r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$')
+            EMAIL_VALIDATION    = r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+            PASSWORD_VALIDATION = r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
             
             email               = user_data['email']
             password            = user_data['password']
             name                = user_data['name'],
             phone_number        = user_data['phone_number'],
 
-            if not EMAIL_VALIDATION.match(email):
+            if not re.match(EMAIL_VALIDATION, email):
                 return JsonResponse({'Message' : 'Invalid Email'}, status = 400)
             
-            if not PASSWORD_VALIDATION.match(password):
+            if not re.match(PASSWORD_VALIDATION, password):
                 return JsonResponse({'Message' : 'Invalid Password'}, status = 400)
             
             if User.objects.filter(email = email).exists():
@@ -35,4 +35,18 @@ class SignUpView(View):
             return JsonResponse({'Message' : 'Created'}, status = 201)
         
         except KeyError:
-            return JsonResponse({'Message': 'FAILED'}, status = 400)
+            return JsonResponse({'Message': 'KEY_ERROR'}, status = 400)
+class SigninView(View):
+    def post(self, request):
+        try:
+            user_data_input = json.loads(request.body)
+            email           = user_data_input['email']
+            password        = user_data_input['password']
+            
+            if not User.objects.filter(email = email, password = password).exists():
+                return JsonResponse({"Message" : "Invalid_User"}, status = 401)
+
+            return JsonResponse({"Message" : "Success"}, status = 200)
+        
+        except:
+            return JsonResponse({"Message" : "KEY_ERROR"}, status = 400)
