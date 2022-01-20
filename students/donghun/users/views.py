977a40792f04fb3f4e554e34f91b129430375d58
lee <higgs95@naver.com>
2022-01-20 13:39:32 +0900

    [EDIT] logic and convention

--- students/donghun/users/views.py
+++ students/donghun/users/views.py
@@ -54,18 +54,16 @@ class SignInView(View):
             user_data  = json.loads(request.body)
             user_email = user_data['email']
 
-            if User.objects.filter(email = user_email).exists():
-                password_bcrypt = User.objects.get(email=user_email)
-                check_password  = bcrypt.checkpw(user_data['password'].encode('utf-8'), password_bcrypt.password.encode('utf-8'))
-
-                if check_password :
-                    access_token = jwt.encode({"user_id" : password_bcrypt.id}, SECRET_KEY, ALGORITHM)
-                    return JsonResponse({"access_token" : access_token}, status=200)
-                else:
-                    return JsonResponse({"messeage" : "INVALID_USER"}, status=401)
-
-            else:
+            if not User.objects.filter(email = user_email).exists():
                 return JsonResponse({"messeage" : "INVALID_USER"}, status=401)
+
+            user = User.objects.get(email=user_email)
+
+            if bcrypt.checkpw(user_data['password'].encode('utf-8'), user.password.encode('utf-8')) :
+                access_token = jwt.encode({"user_id" : user.id}, SECRET_KEY, ALGORITHM)
+                return JsonResponse({"access_token" : access_token}, status=200)
+
+            return JsonResponse({"messeage" : "INVALID_USER"}, status=401)
 
         except KeyError:
             return JsonResponse({"messeage" : "KEY_ERROR"}, status=400)
